a79786f95f7275625ee0681da1219c545cb63c88
Idan <User@MacBook-Air.local>
2023-01-10 22:52:40 -0500

    day 2 of homework for boca code

---
+++ day1Homework.js
@@ -0,0 +1,26 @@
+const country = 'United States';
+const continent =  'North America';
+
+console.log(country);
+console.log(continent);
+
+const pasta = "Mom's handcrafted recipe"
+
+console.log(pasta)
+
+
+// "15" === 15 is FALSE, because they are using two different value types
+function checkIfTrue(n1, n2)
+{
+     
+    if (n1 === n2)
+    {
+        console.log("True!! :D")
+    }
+    else
+    {
+        console.log("False!! ):")
+    }
+}
+// console.log()
+checkIfTrue("15", 15)
---
+++ day2Homerwork2.js
@@ -0,0 +1,15 @@
+function sum(n1, n2) {
+    return n1 + n2
+}
+console.log(sum(3, 5))
+
+function squareRoot(n) {
+   return Math.sqrt(n)
+}
+console.log(squareRoot(16)) 
+
+const student = {
+    grade: 98,
+    studentId: 9817456675,
+    studentName: "Idan Fischer!" 
+}
